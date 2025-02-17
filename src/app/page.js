e2fa1f2dc2e5a710e219672fc8140f9d783c8e3e
preslaviliev93 <preslaviliev105@gmail.com>
2025-02-17 14:15:47 +0200

    Added functionality

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,27 @@
 import React from 'react'
+import Results from '@/components/Results';
 
-export default function Home() {
+
+const API_KEY = process.env.API_KEY;
+console.log(`FOUND API KEY ${API_KEY}`);
+export default async function Home({searchParams}) {
+  const params = await searchParams;
+  const genre = params?.genre || 'fetchTrending';
+  const res = await fetch(`https://api.themoviedb.org/3${
+    genre === 'fetchTopRated' ? '/movie/top_rated' : '/trending/all/week'
+  }?api_key=${API_KEY}&language=en-US&page=1`);
+
+
+  const data = await res.json();
+  if(!data){
+    throw new Error('Failed to fetch data');
+  }
+  const results = data.results;
+  console.log(data);
+
   return (
-    <div></div>
+    <div>
+      <Results results={results}/>
+    </div>
   )
 }
