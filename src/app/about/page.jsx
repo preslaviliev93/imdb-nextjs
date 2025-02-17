f3f19a827f4bd67e43a9faf6d23f21af0f514c38
preslaviliev93 <preslaviliev105@gmail.com>
2025-02-17 12:32:02 +0200

    Create the "About" page

---
+++ src/app/about/page.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function About() {
+  return (
+    <div className="max-w-6xl mx-auto p-3 space-y-4">
+        <h1 className="text-2xl font-medium text-amber-600">About</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas magnam distinctio vel animi harum quidem sapiente, delectus, ut tenetur maxime alias odio corrupti fugit aut excepturi aspernatur consequuntur ducimus vero quia laudantium voluptate. Placeat, earum ipsa, delectus dolorum deserunt mollitia ipsum sapiente soluta, possimus ab suscipit corporis adipisci autem ad?</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim aut sit quam veniam perferendis totam dignissimos reprehenderit odio quaerat. Dolore natus aspernatur doloremque tempora debitis atque totam ratione voluptatibus non consequuntur molestias tenetur, fuga adipisci? Sunt veritatis quasi tempora, repellat, quis, consectetur veniam consequatur eius eligendi fugiat iure at.</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus maiores quod iste accusamus deserunt quaerat amet, incidunt impedit, sequi, atque nobis minus corrupti molestias aliquam expedita a excepturi laboriosam obcaecati!</p>
+    </div>
+  )
+}
